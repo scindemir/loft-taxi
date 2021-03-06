759de9c00eb764233eab76f5866515f751c24157
Sasha Cindemir <scindmr@gmail.com>
2021-03-06 15:36:25 +0300

    fixed the names

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-jest.mock('./Home', () => ({ Home: () => <div>Home component</div> }));
+jest.mock('./Home', () => ({ HomeWithAuth: () => <div>Home component</div> }));
 jest.mock('./Map', () => ({ Map: () => <div>Map component</div> }));
-jest.mock('./Profile', () => ({ Profile: () => <div>Profile component</div> }));
+jest.mock('./Profile', () => ({ ProfileWithAuth: () => <div>Profile component</div> }));
 
 describe("App", () => {
   it("renders correclty", () => {
